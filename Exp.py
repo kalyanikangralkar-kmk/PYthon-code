12d71c8cd85949189498bb32461326eee133df96
Kalyani Kangralkar <kalyanikangralkar@gmail.com>
2026-03-17 19:40:47 +0530

    Add files via upload

---
+++ Exp.py
@@ -0,0 +1,81 @@
+# Experiment 1
+a=23
+print(a)
+print(type(a))
+b=3.4
+print(b)
+print(type(b))
+c="abcdef"
+print(c)
+print(type(c))
+d=True
+print(d)
+print(type(d))
+e=[1,2,3]
+print(e)
+print(type(e))
+f=(1,2,3)
+print(f)
+print(type(f))
+g={4,5,6}
+print(g)
+print(type(g))
+h=(["apple","mango","banana"])
+print(h)
+print(type(h))
+
+# Experiment 2
+# 1
+x=10
+y=20
+z=x+y
+print(z) 
+
+# 2
+a=20
+print(a)
+print(type(a))
+b=4.4
+print(b)
+print(type(b))
+c="kalyani"
+print(c)
+print(type(c))
+d=True
+print(d)
+print(type(d))
+e=[1,2,3]
+print(e)
+print(type(e))
+f=(1,2,3)
+print(f)
+print(type(f))
+g={4,5,6}
+print(g)
+print(type(g))
+
+# 3
+a=float(a)
+print(a)
+print(type(a))
+b=int(b)
+print(b)
+print(type(b))
+c=list(c)
+print(c)
+print(type(c))
+d="abcdef"
+d=tuple(d)
+print(d)
+print(type(d))
+e="xyzpqr"
+e=set(e)
+print(e)
+print(type(e))
+
+# 4
+L=6
+I=8
+print(L<I and L>I)
+print(L<I or L>I)
+print(not L<I)
