f2dc2806b4c70f9d76222c89f064a34a2ee29934
Kalyani Kangralkar <kalyanikangralkar@gmail.com>
2026-04-03 20:00:34 +0530

    Add files via upload

---
+++ Assign5.py
@@ -0,0 +1,17 @@
+#Assignment 5 
+#1
+a=input("Enter a string :")
+print("Length of string is ",len(a))
+
+#2
+b=input("Enter a string :")
+c=b[:2]
+d=b[-2:]
+e=c+d
+print(e)
+
+#3
+x=input("Enter a string :")
+y=input("Enter a string :")
+z=x +y
+print(z)
