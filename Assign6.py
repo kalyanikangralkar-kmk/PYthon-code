0a4f19e91113690095fcaf0573c093edcf984d2f
Kalyani Kangralkar <kalyanikangralkar@gmail.com>
2026-04-03 20:01:45 +0530

    Add files via upload

---
+++ Assign6.py
@@ -0,0 +1,44 @@
+#Assignment 6
+#1
+lst=[1,2,3,4,'a','b','c','d']
+print(lst)
+print(3 in lst)
+print("d" not in lst)
+
+#2
+print(lst[2])
+print(lst[-3])
+print(lst[1:5])
+
+#3
+a=["apple","blueberry","orange"]
+print(a)
+a[2]="123"
+print(a)
+
+a.append("fruits")
+print(a)
+
+a.insert(1,"mango")
+print(a)
+
+a.remove("blueberry")
+print(a)
+
+#4
+x=[1,2,3,4]
+y=[5,6,7,8]
+z=x+y
+print(z)
+
+print(x*4)
+
+abc=["a","b","c","d","e"]
+print(len(abc))
+
+A=["d","c","z","e","a"]
+print(max(A))
+
+B=[5,6,3,8,9,4,6]
+print(min(B))
+
