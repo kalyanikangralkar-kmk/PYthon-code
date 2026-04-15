31c7d34cba80d82a5639391b7ab19e83535146dd
Kalyani Kangralkar <kalyanikangralkar@gmail.com>
2026-04-15 14:06:22 +0530

    Add files via upload

---
+++ Assign7.py
@@ -0,0 +1,34 @@
+#1
+a=(1,2,3,4,5, "a", "b", "c")
+b=(9,8,7,6,5, "e", "f")
+print(type(a))
+
+print(len(a))
+c=a+b
+print(c)
+d=b*2
+print(d)
+print("c" in (a))
+
+#2
+print(a.index(4))
+print(a[3])
+print(a[-2])
+print(a[1:5])
+for a in (1,2,3,4,5,"a", "b", "c"): print(a)
+
+#3
+#a.append(2) 
+#print(a)
+#del a[3] 
+#print(a)
+
+#4
+print(len(c))
+y=(1,2,3,5,6,7)
+print(max(y))
+
+x=(9,5,7,8,2,8)
+print(min(x))
+
+print(x)
